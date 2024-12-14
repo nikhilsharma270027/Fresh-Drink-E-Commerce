286a40030309dc157cd8d3ab460ccdc7456229db
nikhil <nikhilsharma270027@gmail.com>
2024-12-14 23:58:16 +0530

    update flavors,

--- frontend/src/components/Flavors.tsx
+++ frontend/src/components/Flavors.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 interface Product {
   _id: string;
   name: string;
